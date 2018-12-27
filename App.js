8cedb12aa9e8bec08d70ad459d8b147f7e5840e8
Valentin Chassignol <Vinetosdev@gmail.com>
2018-12-27 11:22:43 +0100

    ✏️ Update Icon

--- App.js
+++ App.js
@@ -16,15 +16,14 @@ const instructions = Platform.select({
 });
 
 type Props = {};
-export default class App extends Component < Props > {
+export default class App extends Component <Props> {
     render() {
-        return ( <
-            View style = { styles.container } >
-            <
-            Text style = { styles.welcome } > Welcome to React Native! < /Text> <
-            Text style = { styles.instructions } > To get started, edit App.js < /Text> <
-            Text style = { styles.instructions } > { instructions } < /Text> <
-            /View>
+        return (
+            <View style={styles.container}>
+                <Text style={styles.welcome}> Welcome to React Native! </Text>
+                <Text style={styles.instructions}> To get started, edit App.js </Text>
+                <Text style={styles.instructions}> {instructions}</Text>
+            </View>
         );
     }
 }
